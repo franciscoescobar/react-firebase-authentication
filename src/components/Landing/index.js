49e6301897b42223e72964baccb87d34ea80c763
franciscoescobar <franciscoescobar@users.noreply.github.com>
2019-05-02 19:09:30 -0300

    Added password reset and password change, and protected routes with dynamic authorization conditions.

---
+++ src/components/Landing/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Landing = () => (
+  <div>
+    <h1>Landing</h1>
+    <p>The Landing is accessible by every user.</p>
+  </div>
+);
+
+
+export default Landing;
--- src/components/Session/index.js
+++ src/components/Session/index.js
@@ -1,3 +1,5 @@
 import AuthUserContext from './context';
-import withAuthentication from './withAuthentication'
-export { withAuthentication, AuthUserContext };
+import withAuthentication from './withAuthentication';
+import withAuthorization from './withAuthorization';
+
+export { AuthUserContext, withAuthentication, withAuthorization };
